8e53d497d45ebedae97b071cc4c1267806d7f9f8
Ask Somervoll <asksom@gmail.com>
2019-03-09 16:15:27 +0100

    Added method for parsing of training data from file and a simple validation method to check if it works as intended.

--- Importance.py
+++ Importance.py
@@ -1,16 +1,46 @@
-
-
-
-
-
 def random_importance():
-
     ## Shell Function
 
     return 0
 
-def max_expected_value_importance():
 
+def max_expected_value_importance():
     ## shell function
 
     return 0
+
+
+def decision_tree_learning():
+    return
+
+
+def parse_training_data(file_name):
+    """
+    Method reads training data to be used and places it into a list containing string
+    :param file_name: name of training data file to be read.
+    :return:
+    """
+    with open(file_name, "r") as f:
+        return_list = []
+        data = f.readlines()
+        # this is now a list containing strings on the following format:
+        # "a, b, c, d, e, f, g" where all variables are integers.
+        for string in data:
+            temp_list = string.split("	")
+            for i in range(len(temp_list)):
+                temp_list[i] = int(temp_list[i])
+            return_list.append(temp_list)
+    f.close()
+    return return_list
+
+
+def test_parse_training_data():
+    """
+    Validation method to check if parse_training_data works as expected.
+    :param file_name:
+    :return:
+    """
+    print(parse_training_data("training.txt")[0])
+
+
+test_parse_training_data()
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Importance
-Two different versions of the Importance algorithm used for Decisions Tree Learning
+Two different versions of the Importance algorithm used for Decisions Tree Learning, as well as an implementation of the Decision Tree Learning algorithm.
